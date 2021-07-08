3f9bc86452c9924640184cecedabfdda0c1a31dd
Thanya08 <86050656+Thanya08@users.noreply.github.com>
2021-07-08 09:56:50 +0530

    Add files via upload

--- GameApp/Program.cs
+++ GameApp/Program.cs
@@ -15,26 +15,41 @@ namespace GameApp
             BASIC = 1, INTERMEDIATE, ADVANCED
         }
         static void Main(string[] args)
+
         {
+            int cnt = 0;
             Console.WriteLine("Word Guess Game");
-            
-            
+            inputt();
+            void inputt()
+            {
                 string message = string.Format("Enter Your Choice {0}->Basic , {1}->Intermediate ,{2}->Advanced", Options.BASIC, Options.INTERMEDIATE, Options.ADVANCED);// 1->Basic,2->Intermediate
                                                                                                                                                                          //String Interpollation 
                 string displayMessage = $"Enter Your Choice {(int)Options.BASIC}->Basic,{(int)Options.INTERMEDIATE}->Intermediate,{(int)Options.ADVANCED}->Advanced";
                 Console.WriteLine(displayMessage);
-                Options _choice = (Options)Int32.Parse(Console.ReadLine());
-
-            switch (_choice)
-            {
-                case Options.BASIC:
+                int value = Int32.Parse(Console.ReadLine());
+                //Options _choice = Options.value;
+                if (value >= 4)
+                {
+                    cnt = cnt + 1;
+                    if(cnt<=2)
+                    { 
+                    Console.WriteLine("Invalid choice");
+                    inputt();
+                        
+                     }
+                    return;
+                    
+                }
+                switch (value)
+                {
+                    case 1:
 
                         Console.WriteLine("Basic Level");
                         //Use Reflection  
                         //Assembly Load
                         System.Reflection.Assembly basicLevelLib =
-          System.Reflection.Assembly.LoadFile(@"C:\Users\thanya.n\source\repos\Example\LevelLibs\IntermediateLevelLib.dll");
-                        // Search For Class - BasicLevelType
+          System.Reflection.Assembly.LoadFile(@"C:\Users\thanya.n\source\repos\Example\LevelLibs\BasicLevelLib.dll");
+                        // Search For Class - BasicLevelT
                         System.Type basicLevelTypeClassRef = basicLevelLib.GetType("BasicLevelLib.BasicLevelType");
                         
                         if (basicLevelTypeClassRef != null)
@@ -59,7 +74,7 @@ namespace GameApp
 
                         }
                         break;
-                    case Options.INTERMEDIATE:
+                    case 2:
                         Console.WriteLine("Intermediate Level");
                         System.Reflection.Assembly intermediateLevelLib =
           System.Reflection.Assembly.LoadFile(@"C:\Users\thanya.n\source\repos\Example\LevelLibs\IntermediateLevelLib.dll");
@@ -79,7 +94,7 @@ namespace GameApp
                         }
 
                             break;
-                    case Options.ADVANCED:
+                    case 3:
                         Console.WriteLine("Advanced Level");
                         System.Reflection.Assembly advancedLevelLib =
          System.Reflection.Assembly.LoadFile(@"C:\Users\thanya.n\source\repos\Example\LevelLibs\AdvancedLevelLib.dll");
@@ -99,10 +114,10 @@ namespace GameApp
                         }
                         break;
 
-            }
+                }
 
                 Console.ReadKey();
-            
+            }
         }
     }
 }
